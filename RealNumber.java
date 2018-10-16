fb5986cef9940a7b89842b0699194fe354877793
sxiao1 <sxiao1@stuy.edu>
2018-10-16 10:59:10 -0400

    methods

--- RealNumber.java
+++ RealNumber.java
@@ -1,14 +1,41 @@
-public class RealNumber extends RationalNumber{
-  public RealNumber(){
-    super(0);
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
   }
-  public RealNumber(double x){
-    super(0);
-  }
-  public String toString(){
-    return "" + 0;
-  }
-  public double compareTo(double y, double z){
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
     return 0;
+  }
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return 0;
+  }
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return 0;
+  }
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return 0;
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
   }
 }
