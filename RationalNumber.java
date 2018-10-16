74145cff6475a5d9c26ea3e76b04fa735f44b83f
sxiao1 <sxiao1@stuy.edu>
2018-10-16 00:04:13 -0400

    RationalNumber methods

---
+++ RationalNumber.java
@@ -0,0 +1,18 @@
+public class RationalNumber{
+  private double x;
+  public RationalNumber(){
+    x = 0;
+  }
+  public RationalNumber(double value){
+    x = 0;
+  }
+  public int getX(){
+    return 0;
+  }
+  public String toString(){
+    return "" + 0;
+  }
+  public double compareTo(double y, double z){
+    return 0;
+  }
+}
