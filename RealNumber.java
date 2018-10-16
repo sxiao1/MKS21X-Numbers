40433035e6a8b396a72b72a153699bdb48e5d42a
sxiao1 <sxiao1@stuy.edu>
2018-10-16 00:03:42 -0400

    RealNumber methods

---
+++ RealNumber.java
@@ -0,0 +1,14 @@
+public class RealNumber extends RationalNumber{
+  public RealNumber(){
+    super(0);
+  }
+  public RealNumber(double x){
+    super(0);
+  }
+  public String toString(){
+    return "" + 0;
+  }
+  public double compareTo(double y, double z){
+    return 0;
+  }
+}
